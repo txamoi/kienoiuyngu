f71dda9e2519d90db9a5a71078387bc10c00d9c5
txamoi <yojoadv.alt@gmail.com>
2026-08-30 16:17:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API OK"
+    return jsonify({
+        "status": "online",
+        "message": "API is running"
+    })
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
